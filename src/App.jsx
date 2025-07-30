523376dd629a8a7ca0c61cd902e096e4c40cb34b
Jerryomwesh <jerryomwesh@gmail.com>
2025-07-31 00:00:33 +0300

    rendering main comp

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <div className="sidebar">
+        <Navbar />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
